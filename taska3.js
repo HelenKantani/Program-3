aba79424571569594ccfa0adbc70e2477c4b94a3
HelenKantani <129065421+HelenKantani@users.noreply.github.com>
2023-04-04 16:16:57 +0300

    Add files via upload

---
+++ taska3.js
@@ -0,0 +1,105 @@
+// 1
+// let firstNum = Number(prompt('firstNum'));
+// let secondNum = Number(prompt('secondNum'));
+// let result = 0;
+
+// for (let i = firstNum ; i < secondNum; i++){
+//   result +=i;
+// }
+//   console.log(result);
+
+
+//  //2
+// for (let i = 15 ; i > 0 ; i--) {
+//   console.log(i);
+// }
+
+
+// 3 
+
+// let lines = Number(prompt('Lines'));
+// let starOne = ''; 
+// let star = "*";
+
+//  for(let i=0;i<lines;i++){
+//     starOne+=star
+//     console.log(starOne);
+//    }
+   
+// // 4
+// let height = Number(prompt(" "));
+// let width = Number(prompt(" "));
+// let element = '';
+
+// for(let i = 0; i < height; i++){
+//   let element = '';
+//     for(let j = 0; j < width; j++){
+//     element = element + '*'
+       
+//     }
+//     console.log(element);
+//     console.log(" ");
+// }
+
+   
+// 5
+// let dayWeek = prompt("Days of the week: ");
+
+// switch (dayWeek) {
+//     case "Monday":
+//     case "monday":
+//     case "Wednesday":
+//     case "wednesday":
+//     case "Friday":
+//     case "friday":
+//     case "Saturday":
+//     case "saturday":
+//     case "Sunday":
+//     case "sunday":
+//         console.log('Сегодня можно отдохнуть');
+//         break;
+//     case "Tuesday":
+//     case "tuesday":
+//     case "Thursday":
+//     case "thursday":
+//         console.log('Сегодня учеба');
+//         break;
+//     default:
+//         alert('NO day week');
+//         console.log('No day week');
+//         break;
+// }
+
+
+// 6
+//     let planetSolarSystem  = prompt('Planet');
+
+//     switch (planetSolarSystem) {
+//        case "Mercury" :
+//        case "Venus":
+//        case "Earth" :
+//        case "Mars":
+//        case "Jupiter":
+//        case "Saturn":
+//        case "Uranus":
+//        case "Neptune":
+//             alert('Yes planet');
+//             console.log('Yes planet')
+//         break;
+//     default:
+//         alert('No planet');
+//         console.log('No planet');
+//         break;
+//     }
+
+// 7
+
+// let smile = [':)', '=)',':)', '=)',':)', '=)'];
+
+// for(let i = 0; i < smile.length; i++){
+//    if(smile[i] === '=)'){
+//     smile[i] = ';)'
+//    } 
+   
+// }
+// console.log(smile);
